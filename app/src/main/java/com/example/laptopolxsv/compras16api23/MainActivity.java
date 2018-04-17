f0740e24b1257bd9a709eef38e1031c1cbcc5a6f
kornflakesguy <fer.aravalo1997@gmail.com>
2018-04-16 22:06:20 -0600

    Fondos Nuevos

--- app/src/main/java/com/example/laptopolxsv/compras16api23/MainActivity.java
+++ app/src/main/java/com/example/laptopolxsv/compras16api23/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Esto es una snack bar.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
